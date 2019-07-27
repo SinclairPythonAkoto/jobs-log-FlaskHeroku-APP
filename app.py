b40ea589ed951c94be74775ac7aafdf2341ef823
SinclairPythonAkoto <sinclair.python@gmail.com>
2019-07-27 09:29:51 +0100

    bug fix attempt

--- app.py
+++ app.py
@@ -35,24 +35,27 @@ def before_request():
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
-	if request.method == 'GET':
-		cur = g.db_conn.cursor()
-		entry = cur.execute("SELECT * FROM LogTable;").fetchall()
-		return render_template("homepage.html", entry=entry)
-	else:
-		date_entry = request.form.get("job_date") # Date in LogTable
-		time_entry = request.form.get("job_time") # Time in LogTable
-        job = request.form.get("job_options") # Job in LogTable
-        description = request.form.get("description") # Description in LogTable
-        outcome = request.form.get("job_outcome") # Outcome in LogTable
-        comments = request.form.get("additional_comments") # Comments in LogTable
-        date_stamp = strftime("%d-%m-%y", gmtime()) # Date_Entry in LogTable
-        time_stamp = strftime("%H:%M", gmtime()) # Time_Entry in LogTable
+	# if request.method == 'GET':
+	# 	cur = g.db_conn.cursor()
+	# 	entry = cur.execute("SELECT * FROM LogTable;").fetchall()
+	# 	return render_template("homepage.html", entry=entry)
+	# else:
+	# 	date_entry = request.form.get("job_date") # Date in LogTable
+	# 	time_entry = request.form.get("job_time") # Time in LogTable
+ #        job = request.form.get("job_options") # Job in LogTable
+ #        description = request.form.get("description") # Description in LogTable
+ #        outcome = request.form.get("job_outcome") # Outcome in LogTable
+ #        comments = request.form.get("additional_comments") # Comments in LogTable
+ #        date_stamp = strftime("%d-%m-%y", gmtime()) # Date_Entry in LogTable
+ #        time_stamp = strftime("%H:%M", gmtime()) # Time_Entry in LogTable
         
-        cur.execute("INSERT INTO LogTable (Date_Entry, Time_Entry, Job, Description, Outcome, Comments, Date_Stamp, Time_Stamp VALUES (:date_entry,:time_entry,:job,:description,:outcome,:comments,:date_stamp,time_stamp)", {"Date_Entry":date_entry, "Time_Entry":time_entry, "Job":job, "Description":description, "Outcome":outcome, "Comments":comments, "Date_Stamp":date_stamp, "Time_Stamp":time_stamp})
-        cur.commit()
+ #        cur.execute("INSERT INTO LogTable (Date_Entry, Time_Entry, Job, Description, Outcome, Comments, Date_Stamp, Time_Stamp VALUES (:date_entry,:time_entry,:job,:description,:outcome,:comments,:date_stamp,time_stamp)", {"Date_Entry":date_entry, "Time_Entry":time_entry, "Job":job, "Description":description, "Outcome":outcome, "Comments":comments, "Date_Stamp":date_stamp, "Time_Stamp":time_stamp})
+ #        cur.commit()
         
-        return redirect(url_for("home"))
+ #        return redirect(url_for("home"))
+    cur = g.db_conn.cursor()
+    entry = cur.execute("SELECT * FROM LogTable;").fetchall()
+    return render_template('homepage.html')
 
 
 if __name__ == '__main__': app.run(debug=True)
